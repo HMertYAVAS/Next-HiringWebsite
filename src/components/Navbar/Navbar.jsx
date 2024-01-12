bf6add6849ac71a734a0da6ed48efa7e384037b9
HMertYAVAS <hmertyavas@hotmail.com>
2024-01-13 00:19:44 +0300

    Sticky navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import Logo from "./Logo";
 
 function Navbar() {
     return (
-        <nav class="bg-white dark:bg-gray-900 w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        <nav class="bg-white sticky dark:bg-gray-900 w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 
             <Logo />
